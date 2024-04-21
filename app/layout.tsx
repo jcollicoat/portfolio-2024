d7d3b839f42ddf4643839f87c2f46d0fc76abca3
Joseph Collicoat <jcollicoat@gmail.com>
2024-04-21 16:29:41 +1200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="../public/favicon.png" key="" />
+            </head>
             <body>{children}</body>
         </html>
     );
