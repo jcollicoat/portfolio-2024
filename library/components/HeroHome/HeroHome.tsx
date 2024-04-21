f78c32964bc7dcf726d782b72e669b0398121fe4
Joseph Collicoat <jcollicoat@gmail.com>
2024-04-21 21:57:50 +1200

    Map content to hero home

--- library/components/HeroHome/HeroHome.tsx
+++ library/components/HeroHome/HeroHome.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link';
 import { FC } from 'react';
+import { PageContent } from '@cms/types/sections';
 import { Grid } from '@components/Grid/Grid';
 import { LottieWrapper } from '@components/LottieWrapper/LottieWrapper';
 import collicoat from '@lotties/collicoat.json';
 import joseph from '@lotties/joseph.json';
 import styles from './HeroHome.module.scss';
 
-export const HeroHome: FC = () => {
+interface Props extends PageContent {
+    _type: 'hero_home';
+    heading: string;
+    intro: string;
+    cta: string;
+}
+
+export const HeroHome: FC<Props> = ({ heading, intro, cta }) => {
     return (
         <section className={styles.section}>
             <Grid>
@@ -14,6 +23,28 @@ export const HeroHome: FC = () => {
                 </div>
                 <div className={styles.collicoat}>
                     <LottieWrapper lottie={collicoat} />
+                </div>
+                <div className={styles.intro}>
+                    <h1>{heading}</h1>
+                    <p>{intro}</p>
+                    <Link href="#projects" className={styles.cta}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="100%"
+                            height="100%"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className={styles.icon}
+                        >
+                            <line x1="12" y1="5" x2="12" y2="19"></line>
+                            <polyline points="19 12 12 19 5 12"></polyline>
+                        </svg>
+                        {cta}
+                    </Link>
                 </div>
             </Grid>
         </section>
--- library/sanity/ContentMapper.tsx
+++ library/sanity/ContentMapper.tsx
@@ -1,4 +1,4 @@
-import { FC, useMemo } from 'react';
+import { ComponentProps, FC, useMemo } from 'react';
 import { HeroHome } from '@components/HeroHome/HeroHome';
 import { PageContent } from './types/sections';
 
@@ -15,7 +15,12 @@ export const ContentMapper: FC<Props> = ({ sections }) => {
         return sections.map((section) => {
             switch (section._type) {
                 case 'hero_home':
-                    return <HeroHome />;
+                    return (
+                        <HeroHome
+                            key={section._type}
+                            {...(section as ComponentProps<typeof HeroHome>)}
+                        />
+                    );
             }
         });
     }, [sections]);
